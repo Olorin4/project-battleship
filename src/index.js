20f7478037294ab230992dbc37ba4c198b7fbbd2
Nick Kalas <nick.kalas@proton.me>
2024-12-06 15:20:02 +0200

    Update todo list

--- src/index.js
+++ src/index.js
@@ -25,4 +25,5 @@ document.addEventListener("DOMContentLoaded", () => {
 // TO DO:
 /*  - Implement drag and drop to allow players to place their ships.
     - Create a 2-player option that lets users take turns by passing the laptop back and    forth, - or by spinning the monitor around on a desktop. Implement a ‘pass device’ screen so that players don’t see each other’s boards!
-    - Polish the intelligence of the computer player by having it try adjacent slots after  getting a ‘hit’. */
+    - Polish the intelligence of the computer player by having it try adjacent slots after  getting a ‘hit’. 
+*/
